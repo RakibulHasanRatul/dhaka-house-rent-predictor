b93eb9be93cbd45f5b4e1cecf6ca03ca3db4e901
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2025-06-30 02:00:16 +0600

    rename symbol

--- c_implementations/c_pthread/matrix_ops_pthread.h
+++ c_implementations/c_pthread/matrix_ops_pthread.h
@@ -1,5 +1,5 @@
-#ifndef MATRIX_OPS_OMP_H
-#define MATRIX_OPS_OMP_H
+#ifndef MATRIX_OPS_PTHREAD_H
+#define MATRIX_OPS_PTHREAD_H
 void free_matrix(double **matrix, const int rows);
 double **inverse_matrix(const double *const *matrix, const int size);
 double **multiply_matrices(const double *const *mat_a, const double *const *mat_b, const int rows_a,
